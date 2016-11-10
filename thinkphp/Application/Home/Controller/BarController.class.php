5ac626339b35d90487b771d3359831c58b123b6e
s53yjx <1476002733@qq.com>
2016-11-10 23:36:47 +0800

    yjx day1

---
+++ thinkphp/Application/Home/Controller/BarController.class.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Home\Controller;
+use Think\Controller;
+
+class BarController extends Controller
+{
+    public function index()
+    {
+        $this->display();
+    }
+}
+
+
+
