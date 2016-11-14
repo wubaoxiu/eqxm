33cb8ee899b808fb86989db2f98549fbff89ec2f
ONLY LOVE <nick>
2016-11-14 09:51:34 +0800

    xdm update day3

--- thinkphp/Application/Admin/Controller/UserController.class.php
+++ thinkphp/Application/Admin/Controller/UserController.class.php
@@ -3,22 +3,14 @@ namespace Admin\Controller;
 
 class UserController extends AdminController
 {
-    public function _initialize()
-    {
-        parent::_initialize();
-    }
     /*
         获取普通用户列表
     */
     public function index(){
-        $data = array(
-            array('id'=>1,'name'=>'bx','email'=>'123@163.com'),
-            array('id'=>2,'name'=>'dm','email'=>'124@163.com'),
-
-        );
+        $data = M('user')->field('u.id,u.name,u.sex,u.email,r.role')->table('csw_user u,csw_role r')->where('u.role_id=r.id')->select();
+        // var_dump($data);
         $this->assign('userlist',$data);
         $this->assign('title','普通用户列表');
-        $this->assign('name','吴宝秀');
         $this->display('User/index');
     }
 
