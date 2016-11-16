f82fd8664863797a6ef4cf448c4535d63585dc82
bb <1075796211@qq.com>
2016-11-16 14:43:09 +0800

    wbx 11-16 one

--- thinkphp/Application/Admin/Model/BarinfoModel.class.php
+++ thinkphp/Application/Admin/Model/BarinfoModel.class.php
@@ -3,23 +3,23 @@ namespace Admin\Model;
 
 use \Think\Model;
 
-class BarinfoModel extends Model{
-    protected $_validate = array(
-        // array(验证字段1,验证规则,错误提示,[验证条件,附加规则,验证时间]),
-        array('name','require','吧名不能为空！'),
-        array('title','0,60','宣言不多于20字',0,'length'),
-        array('name','','贴吧名称已经存在！',0,'unique'), // 在新增、修改的时候验证name字段是否唯一
-    );
+// class BarinfoModel extends Model{
+//     protected $_validate = array(
+//         // array(验证字段1,验证规则,错误提示,[验证条件,附加规则,验证时间]),
+//         array('name','require','吧名不能为空！'),
+//         array('title','0,60','宣言不多于20字',0,'length'),
+//         array('name','','贴吧名称已经存在！',0,'unique'), // 在新增、修改的时候验证name字段是否唯一
+//     );
 
-    protected $_auto = array(
-        // array(完成字段1,完成规则,[完成条件,附加规则]),
-        array('createtime','time',1,'function'),
-        array('user_id','getUserId',1,'callback'),
-    );
+//     protected $_auto = array(
+//         // array(完成字段1,完成规则,[完成条件,附加规则]),
+//         array('createtime','time',1,'function'),
+//         array('user_id','getUserId',1,'callback'),
+//     );
 
-    protected function getUserId()
-    {
-        $id = $_SESSION['admin_info']['id'];
-        return $id;
-    }
-}
+//     protected function getUserId()
+//     {
+//         $id = $_SESSION['admin_info']['id'];
+//         return $id;
+//     }
+// }
