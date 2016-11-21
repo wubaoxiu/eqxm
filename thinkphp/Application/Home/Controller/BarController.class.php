2649f13f6c86f3a42b37fa0437f40474cc45336c
s53yjx <1476002733@qq.com>
2016-11-21 23:06:46 +0800

    yjx 11/21

--- thinkphp/Application/Home/Controller/BarController.class.php
+++ thinkphp/Application/Home/Controller/BarController.class.php
@@ -3,10 +3,46 @@
 namespace Home\Controller;
 use Think\Controller;
 
+/**
+    *前台贴吧控制器 BarController
+    *控制器作用    某个贴吧首页的遍历
+    *@作者    yjx
+    *@date    2016/11/21
+*/
+
 class BarController extends Controller
 {
+    // 定义数据库操作类
+    private $_barinfo = null;
+    private $_note = null;
+    private $_user = null;
+
+    // 初始化
+    public function _initialize()
+    {
+        $this->_barinfo = M('barinfo');
+        $this->_note = M('note');
+        $this->_user = M('user');
+    }
+
+    // 主页
     public function index()
     {
+        $id = 1;
+        $data = $this->_barinfo->where(array('id'=>array('eq',$id)))->find();
+        // dump($data);
+        if(empty($data)){
+            U('index/index');exit;
+        }
+        $list = $this->_note->where(array('bar_id'=>array('eq',$id)))->order('id desc')->select();
+        foreach ($list as $k=>$v) {
+            // dump($v);
+            $list[$k]['louzhu'] = $this->_user->where(array('id'=>array('eq',$v['user_id'])))->find();
+        }
+        // dump($list);die;
+        $this->assign('list',$list);
+        $this->assign('louzhu',$louzhu);
+        $this->assign('data',$data);
         $this->display();
     }
     public function note()
---
+++ thinkphp/Application/Home/Controller/NoteController.class.php
@@ -0,0 +1,48 @@
+<?php 
+namespace Home\Controller;
+use Think\Controller;
+/**
+    *前台贴吧控制器 LoginController
+    *控制器作用    贴吧帖子的操作
+    *@作者    yjx
+    *@date    2016/11/21
+*/
+class NoteController extends Controller
+{
+    // 定义数据库操作类
+    private $_note = null;
+
+    // 初始化
+    public function _initialize()
+    {
+        $this->_note = M('note');
+    }
+
+    // 帖子添加
+    public function add()
+    {
+        session_start();
+        date_default_timezone_set("PRC");
+        if(empty($_SESSION['user'])){
+            $this->error('您还没有登录，请先登录！',U('Login/index'));
+        }
+        // dump($_POST['content']);
+        $data['user_id'] = $_SESSION['user']['id'];
+        $data['bar_id'] = $_POST['id'];
+        $data['title'] = $_POST['title'];
+        $data['createtime'] = time();
+        $data['isfine'] = 1;
+        $data['is_show'] = 1;
+        $data['scan'] = 0;
+        $data['reply'] = 0;
+        $data['content'] = $_POST['content'];
+        if($this->_note->data($data)->add()>0){
+            $this->success('发帖成功！',U('Bar/index'));
+        }else{
+            $this->error('发帖失败！');
+        }
+    }
+    
+}
+
+
